b5506d2fa95f370cd96ec56b58b1a3a4d5cabcb6
EhoodGoren <ehoodgoren@gmail.com>
2022-01-02 17:23:48 +0200

    Exercise 4

--- answers.sql
+++ answers.sql
@@ -17,3 +17,9 @@ SELECT *
  ORDER BY year_of_joining, month_of_joining, day_of_joining
  LIMIT 10
 --  asnwer: Otha Orrell
+
+-- Exercise 4
+SELECT first_name, last_name, age_in_years, city, state
+ FROM emp
+ WHERE first_name = 'Jack'
+--  answer: 4
