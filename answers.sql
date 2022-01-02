de87a0ab1797fdf029d036a82a808aac458973f2
EhoodGoren <ehoodgoren@gmail.com>
2022-01-02 18:35:46 +0200

    Exercise 13

--- answers.sql
+++ answers.sql
@@ -92,3 +92,15 @@ SELECT *,
  END AS Explained_Age
 FROM emp
 LIMIT 100
+
+-- Exercise 13
+SELECT *,
+ CASE
+  WHEN years_in_company < 5 THEN 'Rookie'
+  WHEN years_in_company BETWEEN 5 AND 15 THEN 'Experienced'
+  WHEN years_in_company > 15 THEN 'Old'
+  WHEN years_in_company > 15 AND name_prefix = 'Dr.' THEN 'Medical expert'
+ END AS Explained_company_experience
+FROM emp
+ORDER BY years_in_company
+LIMIT 1000
