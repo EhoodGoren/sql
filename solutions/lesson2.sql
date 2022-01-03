830f2bae3771d77b174308551d63e5f2f4c5a31d
EhoodGoren <ehoodgoren@gmail.com>
2022-01-03 11:32:57 +0200

    Exercise 2

--- solutions/lesson2.sql
+++ solutions/lesson2.sql
@@ -1,5 +1,12 @@
+-- Exercise 1
 SELECT country, COUNT(event) as cnt
  FROM summer
  GROUP BY country
  HAVING cnt > 100
  ORDER BY cnt DESC
+
+-- Exercise 2
+SELECT medal, GROUP_CONCAT(athlete), COUNT(athlete)
+ FROM summer
+ WHERE country='CAN'
+ GROUP BY medal
