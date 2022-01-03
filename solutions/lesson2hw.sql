64cd1b3ca192b4f94bfcc46f0f0705fbb184b30c
EhoodGoren <ehoodgoren@gmail.com>
2022-01-03 22:22:31 +0200

    Exercise 2

--- solutions/lesson2hw.sql
+++ solutions/lesson2hw.sql
@@ -3,3 +3,9 @@ SELECT duration
  FROM movies
  WHERE title = 'Outback'
 -- Answer: 86
+
+-- Exercise 2
+SELECT title
+ FROM movies
+ WHERE year = '2017' AND director = 'Peter Sullivan'
+-- Answer: The Sandman
